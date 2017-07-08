9c371aa0bcca3c9415b00efe12c35c4138dc138b
Jason Laning <jason.laning@gmail.com>
2017-07-07 22:05:02 -0400

    enabled cors

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/*', (req, res) => {
 	res.json({ok: true});
 })
