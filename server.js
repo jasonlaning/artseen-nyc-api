3b52012c1617c9f57274a79739d0a579cd98bd56
Jason Laning <jason.laning@gmail.com>
2017-07-08 22:19:50 -0400

    added api routing to new endpoints

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const {router: ApiRouter} = require('./router');
+
+const {PORT, DATABASE_URL} = require('./config');
+
+mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -9,10 +16,46 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/api/*', (req, res) => {
-	res.json({ok: true});
-})
+app.use(morgan('common'));
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.use('/api/', ApiRouter);
 
-module.exports = {app};
+let server;
+
+function runServer(databaseUrl, port=PORT) {
+	return new Promise((resolve, reject) => {
+		mongoose.connect(databaseUrl, err => {
+			if (err) {
+				return reject(err);
+			}
+			server = app.listen(port, () => {
+				console.log(`Your app is listening on port ${port} and using database ${databaseUrl}`);
+				resolve();
+			})
+			.on('error', err => {
+				mongoose.disconnect();
+				reject(err);
+			});
+		});
+	});
+}
+
+function closeServer() {
+	return mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing server');
+			server.close(err => {
+				if (err) {
+					return reject(err);
+				}
+			resolve();
+			});
+		});
+	});
+}
+
+if (require.main === module) {
+	runServer(DATABASE_URL).catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
